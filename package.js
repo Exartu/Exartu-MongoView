17a66845c83b0b5df57861e2d32d536c2c4beab1
javier <javier.berneche@gmail.com>
2015-01-13 14:27:41 -0200

    fix underscore

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "view for mongo",
   name: 'aida:mongo-view',
-  version: "0.1.1",
+  version: "0.1.2",
   git: "https://github.com/Exartu/Exartu-MongoView.git"
 });
 
 Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@0.9.2');
-  api.use('underscore', 'client');
+  api.use('underscore', 'server');
 
   api.addFiles(['server.js'], 'server');
   api.export('View', 'server');
