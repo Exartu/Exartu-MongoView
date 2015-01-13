9ff1009136a9ab5197019e72d5759585be958f2e
javier <javier.berneche@gmail.com>
2015-01-13 13:51:16 -0200

    update package.js

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
 Package.describe({
   summary: "view for mongo",
-  name: 'mongoview'
+  name: 'aida:pagination',
+  version: "0.1.0",
+  git: "https://github.com/Exartu/Exartu-MongoView.git"
 });
 
 Package.onUse(function (api, where) {
